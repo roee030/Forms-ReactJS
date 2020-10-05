dd26011cd4ccebcfa4de20c761817df616e10023
Roee Angel <roee.angel93@gmail.com>
2020-10-05 20:13:12 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./App.css";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import CreateSurvey from "./Components/CreateSurvey";
+import Survey from "./Components/Survey";
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/createsurvey" exact component={CreateSurvey}></Route>
+            <Route path="/survey/:id" component={Survey}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;

29459701f0124435949212c1405b34025133e96d
Roee Angel <roee.angel93@gmail.com>
2020-10-05 20:24:05 +0300

    add route and header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 export default function Header() {
   return (
     <div className="header">
       <Link to="/">Home</Link>
-      <Link to="/createsurvey">Products list</Link>
+      <Link to="/createsurvey">Create Survey</Link>
     </div>
   );
 }

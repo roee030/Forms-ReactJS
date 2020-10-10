490b3609bc299703211f35d63206cf65a44da7fa
Roee Angel <roee.angel93@gmail.com>
2020-10-11 00:35:06 +0300

    add more function TODO: add page of answers

--- src/Components/DisplaySurveyAnswer.js
+++ src/Components/DisplaySurveyAnswer.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 export default function DisplaySurveyAnswer() {
-  const chart = () => {
-    
-
-  useEffect(() => {
-    // chart();
-  }, []);
   return <div>DisplaySurveyAnswer</div>;
 }
